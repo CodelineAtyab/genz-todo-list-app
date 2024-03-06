99a41be48bd950004d9ff555cd9584eb7bd16f3b
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-06 14:04:46 +0400

    did a minor change in the solution

--- examples/alhasan_work_area/genZ_26_even_odd_filter.py
+++ examples/alhasan_work_area/genZ_26_even_odd_filter.py
@@ -1,26 +1,28 @@
-evenList = []
-oddlist = []
-user_input_list = input("Enter your desired list of numbers with a space between them: ")
-input_list = [int(x) for x in user_input_list.split()]
-user_input = input("Enter Even or Odd: ")
-for i in input_list:
-    if i % 2 == 0:
-        evenList.append(i)
-    else:
-        oddlist.append(i)
-if user_input == "Even":
-    print(evenList, "Parity: Even")
-elif user_input == 'Odd':
-    print(oddlist, "Parity: Odd")
-else:
-    print("You have input invalid option")
+#Another approach
+
+# evenList = []
+# oddlist = []
+# user_input_list = input("Enter your desired list of numbers with a space between them: ")
+# input_list = [int(x) for x in user_input_list.split()]
+# user_input = input("Enter Even or Odd: ")
+# for i in input_list:
+#     if i % 2 == 0:
+#         evenList.append(i)
+#     else:
+#         oddlist.append(i)
+# if user_input == "Even":
+#     print(evenList, "Parity: Even")
+# elif user_input == 'Odd':
+#     print(oddlist, "Parity: Odd")
+# else:
+#     print("You have input invalid option")
 
 
-def even_odd_filter(list):
+def even_odd_filter(list_of_nums):
     user_input = input("Enter Even or Odd: ")
     evenList = []
     oddlist = []
-    for i in list:
+    for i in list_of_nums:
         if i % 2 == 0:
             evenList.append(i)
         else:
@@ -32,4 +34,4 @@ def even_odd_filter(list):
     else:
         print("You have input invalid option")
 
-print(even_odd_filter([1,2,3,4,5,6]))
+even_odd_filter([1,2,3,4,5,6])

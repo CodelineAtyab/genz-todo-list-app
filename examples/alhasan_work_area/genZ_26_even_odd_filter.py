7e50629a7cd9079a4eb8cd724d50089a8b3ea9e2
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-06 11:49:41 +0400

    Added my logic of even odd in genz 26

---
+++ examples/alhasan_work_area/genZ_26_even_odd_filter.py
@@ -0,0 +1,35 @@
+evenList = []
+oddlist = []
+user_input_list = input("Enter your desired list of numbers with a space between them: ")
+input_list = [int(x) for x in user_input_list.split()]
+user_input = input("Enter Even or Odd: ")
+for i in input_list:
+    if i % 2 == 0:
+        evenList.append(i)
+    else:
+        oddlist.append(i)
+if user_input == "Even":
+    print(evenList, "Parity: Even")
+elif user_input == 'Odd':
+    print(oddlist, "Parity: Odd")
+else:
+    print("You have input invalid option")
+
+
+def even_odd_filter(list):
+    user_input = input("Enter Even or Odd: ")
+    evenList = []
+    oddlist = []
+    for i in list:
+        if i % 2 == 0:
+            evenList.append(i)
+        else:
+            oddlist.append(i)
+    if user_input == "Even":
+        print(evenList, "Parity: Even")
+    elif user_input == 'Odd':
+        print(oddlist, "Parity: Odd")
+    else:
+        print("You have input invalid option")
+
+print(even_odd_filter([1,2,3,4,5,6]))

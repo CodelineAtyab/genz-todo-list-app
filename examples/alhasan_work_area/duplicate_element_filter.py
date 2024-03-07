048c0672e5be01506c982879a2efc225bd47b47e
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-07 11:55:51 +0400

    fixed the solution as suggested

--- examples/alhasan_work_area/duplicate_element_filter.py
+++ examples/alhasan_work_area/duplicate_element_filter.py
@@ -5,6 +5,6 @@ def remove_duplicate_filter(lst):
             modified_list.append(nums)
     return modified_list
 
-print(remove_duplicate_filter(["ali","ali","alhasan"]))
-print(remove_duplicate_filter([1,1,3,4,5]))
+print(remove_duplicate_filter(["ali", "ali", "alhasan"]))
+print(remove_duplicate_filter([1, 1, 3, 4, 5]))
 

4630584749294bddc93d59d844c9f226a277d0c5
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-07 11:53:30 +0400

    fixed the solution as suggested

--- examples/alhasan_work_area/duplicate_element_filter.py
+++ examples/alhasan_work_area/duplicate_element_filter.py
@@ -1,13 +1,10 @@
 def remove_duplicate_filter(lst):
     modified_list = []
     for nums in lst:
-        if nums in modified_list:
-            modified_list.remove(nums)
-            modified_list.append(nums)
-        else:
+        if nums not in modified_list:
             modified_list.append(nums)
     return modified_list
 
 print(remove_duplicate_filter(["ali","ali","alhasan"]))
-print(remove_duplicate_filter([1,2,3,3,3,4,5]))
+print(remove_duplicate_filter([1,1,3,4,5]))
 

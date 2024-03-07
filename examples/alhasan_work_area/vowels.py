6f5df8a2f27f3d41591b11360c29511efbd8b5e9
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-07 10:40:15 +0400

    fixed a typo in the solution

--- examples/alhasan_work_area/vowels.py
+++ examples/alhasan_work_area/vowels.py
@@ -2,10 +2,8 @@ def replace_vowels(word):
     vowels_list = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
     for char in word:
         if char in vowels_list: 
-            replaced_word = word.replace(char, "-")
-        else:
-            replaced_word = word
-    return replaced_word
+            word = word.replace(char, "-")
+    return word
 
-print(replace_vowels('eee'))
+print(replace_vowels('Hello GenZ!'))
 

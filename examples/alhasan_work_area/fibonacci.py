b801b3bd2188f54a261a933932e6f04f4db1e8d0
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-10 15:02:20 +0400

    GenZ-108: Added solution for the missing element task

--- examples/alhasan_work_area/fibonacci.py
+++ examples/alhasan_work_area/fibonacci.py
@@ -15,3 +15,15 @@ def fibonacci(series):
         return result
 
 print(fibonacci(10))   
+
+
+def missing_element(lst):
+    expected_sum_of_list = 0
+    actual_sum_of_list = 0
+    for i in range(lst[0],lst[-1]+1):
+        expected_sum_of_list = expected_sum_of_list + i
+    for n in lst:
+        actual_sum_of_list += n
+    return (expected_sum_of_list - actual_sum_of_list)
+
+print(missing_element([1,2,3,5,6]))

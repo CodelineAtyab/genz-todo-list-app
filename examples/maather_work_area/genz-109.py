a3ef8238e178e72ef0a0db93282b5f1ededd54e0
Maather Yasse Mohamed AlJabri <71526@omantel.om>
2024-03-11 10:52:31 +0400

    change variable name

--- examples/maather_work_area/genz-109.py
+++ examples/maather_work_area/genz-109.py
@@ -7,9 +7,9 @@ ex : palindrome_structure(5)
     1234321
    123454321
 ''' 
-def palindrome_structure(n):
+def palindrome_structure(num_of_rows_for_triangle):
     temp = 0 #stores the value of the previous num , starting with 0
-    for i in range(0, n + 1):
+    for i in range(0, num_of_rows_for_triangle + 1):
         num = (10**i) + temp 
         temp = num
-        print(f"{' ' * (n - i)}{num*num}")
+        print(f"{' ' * (num_of_rows_for_triangle - i)}{num*num}")

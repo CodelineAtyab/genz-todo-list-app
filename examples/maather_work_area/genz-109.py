fcb9425b062c7739b4086eab1857600fb7ab3c85
Maather Yasse Mohamed AlJabri <71526@omantel.om>
2024-03-11 10:02:57 +0400

    add detailed comments

--- examples/maather_work_area/genz-109.py
+++ examples/maather_work_area/genz-109.py
@@ -1,8 +1,14 @@
 '''
-A function that builds a palindrome structure 
+A function that builds a triangle palindrome structure 
+ex : palindrome_structure(5)
+       1
+      121
+     12321
+    1234321
+   123454321
 ''' 
 def palindrome_structure(n):
-    temp = 0
+    temp = 0 #stores the value of the previous num , starting with 0
     for i in range(0, n + 1):
         num = (10**i) + temp 
         temp = num

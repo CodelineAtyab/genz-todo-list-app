d91986ec03709acb75912a59adaf9d062c1d4740
Sara Yasser Amur Al Shukaili <71524@omantel.om>
2024-03-11 22:42:40 +0400

    Edited solution for XOR swapper

--- examples/sara_work_area/genz_148_XOR_swapper.py
+++ examples/sara_work_area/genz_148_XOR_swapper.py
@@ -3,7 +3,15 @@
 """
 
 def XOR_swapper(value1 , value2):
-    value1, value2 = value2, value1
+    x = value1 ^ value2
+    value1 = x ^ value1
+    value2 = x ^ value2
     print(f"First value is: {value1} Second value is: {value2}")
 
 XOR_swapper(1,3)
+
+# def XOR_swapper(value1 , value2):
+#     value1, value2 = value2, value1
+#     print(f"First value is: {value1} Second value is: {value2}")
+
+# XOR_swapper(1,3)

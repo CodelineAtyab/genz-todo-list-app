072e9bc99a151550224f8c36a2cbf9c45ee31057
abdulaziz ali hamed al badi <71528@omantel.om>
2024-03-11 10:17:18 +0400

    genz-128 pallind

---
+++ examples/abdul_aziz_work_area/palin.py
@@ -0,0 +1,12 @@
+
+user_input = input("Enter a string: ")
+
+
+cleaned_chars = [char.lower() for char in user_input if char.isalpha()]
+cleaned_string = ''.join(cleaned_chars)
+
+
+if cleaned_string == cleaned_string[::-1]:
+    print("True, the string is a palindrome.")
+else:
+    print("False, the string is not a palindrome.")

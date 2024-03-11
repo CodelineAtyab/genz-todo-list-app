8b7f2cb7ba6fb1afcd685c26dc08abe8e42f444f
Ziyad Abdullah Saleh Al Hashar <71534@omantel.om>
2024-03-11 09:59:10 +0400

    solution to 103

--- examples/ziyad_work_area/genz103.py
+++ examples/ziyad_work_area/genz103.py
@@ -6,14 +6,8 @@ input = input.replace(".", "")
 input = input.replace(":", "")
 input = input.replace(";", "").lower()
 
-is_palindrome = True
-start_index = 0
-end_index = len(input) - 1
-
-for i in range(int(end_index / 2)):
-    if not input[start_index] == input[end_index]:
-        is_palindrome = False
-    start_index += 1
-    end_index -= 1
-
-print(is_palindrome)
+input_reverse = input[::-1]
+if input == input_reverse:
+    print(True)
+else:
+    print(False)

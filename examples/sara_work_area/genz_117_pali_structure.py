b7b55b56c44bcde2735dedbc5b6ef403952f50b9
Sara Yasser Amur Al Shukaili <71524@omantel.om>
2024-03-11 09:56:54 +0400

    Updated solution for palindrome structure

--- examples/sara_work_area/genz_117_pali_structure.py
+++ examples/sara_work_area/genz_117_pali_structure.py
@@ -1,10 +1,24 @@
-value = int(input("Please enter the number of lines: "))
+"""
+A program to take number of lines from the user
+and output a palindrome triangle structure
+"""
 
-def pali_structure(value):
-    x=""
-    for i in range(1,value+1):
-        x = x + "1"
-        space = " " * (value-i)
-        print(space, (int(x) * int(x)))
+no_rows = int(input("Please enter the number of lines: "))
+
+def generate_palindrom_triangle(no_rows):
+    added_value = 0
+    for i in range(0,no_rows+1):
+        value = (10**i) + added_value
+        added_value = value
+        space = " " * (no_rows-i)
+        print(space, (value*value))
     
-pali_structure(value)
+generate_palindrom_triangle(no_rows)
+
+# # Solution 2
+# def generate_palindrom_triangle(no_rows):
+#     string_of_ones=""
+#     for i in range(1,no_rows+1):
+#         string_of_ones += "1"
+#         space = " " * (no_rows-i)
+#         print(space, (int(string_of_ones) * int(string_of_ones)))

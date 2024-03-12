57dde581f2cba4e4786a5a1507bf5a2a32143540
Sughiya-AlSaid <162094061+Sughiya-AlSaid@users.noreply.github.com>
2024-03-12 11:34:20 +0400

    genz134 pali verification 3

--- examples/Sughiyas_work_area/genz_134_Palindrome_Verification_Tool.py
+++ examples/Sughiyas_work_area/genz_134_Palindrome_Verification_Tool.py
@@ -1,7 +1,8 @@
 a = input("Enter a string to check if palindrome: ")
-b = a[::-1]
+a1="".join(a.split())
+a2=a1[::-1]
 
-if b==a[::-1]:
+if a2.lower()==a1.lower():
     print("True")
 else:
-    print("False")    
+    print("False") 

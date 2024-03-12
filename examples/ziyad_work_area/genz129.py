80a2eb319c7104440e0989581bfccaf712c19136
Ziyad Abdullah Saleh Al Hashar <71534@omantel.om>
2024-03-12 11:13:18 +0400

    Solution to Genz 129

--- examples/ziyad_work_area/genz129.py
+++ examples/ziyad_work_area/genz129.py
@@ -1,11 +1,14 @@
 input = [5, 1, 9, 7, 2, 8, 5, 11, 14, 33, 38, 6]
 
-largest = input[0]
-second_largest = input[0]
+largest = -999999
+second_largest = -999999
 
 for i in input:
     if i > largest:
         second_largest = largest
         largest = i
+    elif i > second_largest and largest > second_largest:
+        second_largest = i
+
 
 print(second_largest)

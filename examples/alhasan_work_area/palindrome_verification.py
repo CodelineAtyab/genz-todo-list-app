ac44989c0a7619a19dd66636c75e5a7a9bbc4536
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-12 08:36:54 +0400

    Genz-130: minor change in the palindrome verification task

--- examples/alhasan_work_area/palindrome_verification.py
+++ examples/alhasan_work_area/palindrome_verification.py
@@ -1,3 +1,8 @@
+"""
+A function to check if a given string is a palindrome and returns a
+boolean value indicating whether or not the string is a palindrome.
+
+"""
 def palindrome_verification(word):
     new_word = word.replace(" ", "").replace(",", "").lower()
     reversed_word = new_word[::-1]

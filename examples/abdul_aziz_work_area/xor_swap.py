a6605efafe5b2d3efdbad89665042e87eafde041
abdulaziz ali hamed al badi <71528@omantel.om>
2024-03-12 08:22:19 +0400

    xor file added

---
+++ examples/abdul_aziz_work_area/xor_swap.py
@@ -0,0 +1,8 @@
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+print(f"before xor operations num1 ={num1}, num2 = {num2}")
+num1 = num1 ^ num2
+num2 = num1 ^ num2
+num1 = num1 ^ num2
+
+print(f"before xor operations num1 ={num1}, num2 = {num2}")

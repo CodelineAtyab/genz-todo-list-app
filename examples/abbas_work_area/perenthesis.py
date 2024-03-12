20a0e604afa040dc8a40f791c3c7e0cd36c25c94
Abbas Tawfiq Ali Abdulrab <71519@omantel.om>
2024-03-12 11:46:49 +0400

    perenthesis

--- examples/abbas_work_area/perenthesis.py
+++ examples/abbas_work_area/perenthesis.py
@@ -3,22 +3,19 @@ a tool that verifies if perenthesis are properly closed or not
 
 """
 
-
 def perenthesis(user_input):
-    brackets_start = "("
-    bracket_end = ")"
-    check = []
-    check2 = []
+    stack = []
+    result = ""
+    for char in user_input:
+        if char == "(":
+            stack.append(char)
+        elif char == ")":
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
 
-    for i in user_input:
-        if i == brackets_start:
-            check.append(i)
-        elif i == bracket_end:
-            check2.append(i)
-        
-    if len(check) == len(check2):
-        print("True")
-    else:
-        print("False")
 
-perenthesis("(())")
+print(perenthesis("))(("))
+print(perenthesis("()"))
+print(perenthesis(")"))

ab1915cd82bb01e1e079b1d24b4ea59c7d9e3767
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2024-03-12 11:29:00 +0400

    Updated the app and removed .pyc files

--- examples/atyab_work_area/todo_list_application/file_manager.py
+++ examples/atyab_work_area/todo_list_application/file_manager.py
@@ -1,9 +1,18 @@
+import traceback
+
 REMINDER_FILE_PATH = "./data/stored_reminders.txt"
 
+
 def store_reminder(incoming_reminder):
-  with open(REMINDER_FILE_PATH, "a") as reminder_file:
-          reminder_file.write(incoming_reminder + "\n")
+    try:
+        with open(REMINDER_FILE_PATH, "a") as reminder_file:
+            reminder_file.write(incoming_reminder + "\n")
+    except FileNotFoundError:
+        print("Unable to locate file:", REMINDER_FILE_PATH)
+    except Exception:
+        print("Something went wrong.", traceback.format_exc())
+
 
 def read_reminders():
-     with open(REMINDER_FILE_PATH, "r") as reminder_file:
-          return [line.strip() for line in reminder_file.readlines()]
+    with open(REMINDER_FILE_PATH, "r") as reminder_file:
+        return [line.strip() for line in reminder_file.readlines()]

da3bd2e9ba62b0c3abfb7557c30e21fe78f26b43
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-13 00:59:01 +0400

    GenZ-167: Added a solution

---
+++ examples/alhasan_work_area/parentheses_validation_tool.py
@@ -0,0 +1,22 @@
+'''
+
+A function which validate the correct pairing and nesting of 
+opening and closing parentheses in a string using a stack
+
+'''
+
+
+def isValid(parentheses):
+    stack = []
+    pairs_of_parentheses = {")":"("}
+    for pair in parentheses:
+        if pair in pairs_of_parentheses.values():
+            stack.append(pair)
+        elif pair in pairs_of_parentheses.keys():
+            if len(stack) == 0 or stack.pop() != pairs_of_parentheses[pair]:
+                return False
+    return True
+
+
+print(isValid("())"))
+print(isValid("(())"))

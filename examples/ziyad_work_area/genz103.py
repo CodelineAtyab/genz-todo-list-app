e5b9a735c2d12fc2dad3760d778d6a23b73b342b
Ziyad Abdullah Saleh Al Hashar <71534@omantel.om>
2024-03-13 10:14:33 +0400

    Refactor code

--- examples/ziyad_work_area/genz103.py
+++ examples/ziyad_work_area/genz103.py
@@ -1,11 +1,13 @@
-input = input("Enter: ")
+input = input("Enter to check if palindrome: ")
 
+# Remove spaces and punctuation, and convert to lowercase letters
 input = input.replace(" ", "")
 input = input.replace(",", "")
 input = input.replace(".", "")
 input = input.replace(":", "")
 input = input.replace(";", "").lower()
 
+# Reverse string and check equality
 input_reverse = input[::-1]
 if input == input_reverse:
     print(True)

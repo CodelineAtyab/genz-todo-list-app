88c3dc5ff4c949c6755621f5bb6f8b77ef2d81b1
Al-Hasan Ali Khatim Al-Julandani <71536@omantel.om>
2024-03-18 11:24:51 +0400

    Added comments

--- examples/alhasan_work_area/contact_book_management/file_methods.py
+++ examples/alhasan_work_area/contact_book_management/file_methods.py
@@ -4,12 +4,20 @@ FILE_STORAGE_PATH = "./contact.store.csv"
 
 
 def get_individual_fields():
+    """
+    This function is responsible for creating individual fields from the csv file
+    :return:
+    """
     with open(FILE_STORAGE_PATH, "r") as data_store_file:
         reader = [line.strip().split(",") for line in data_store_file.readlines()]
         return reader
 
 
 def create_file_if_not_exist():
+    """
+    This function will create a csv file if it does not exist
+    :return: csv file
+    """
     headers = ",".join(["name", "phone", "email", "address"])
     if not os.path.exists(FILE_STORAGE_PATH):
         with open(FILE_STORAGE_PATH, "w") as data_store_file:
@@ -17,6 +25,14 @@ def create_file_if_not_exist():
 
 
 def add_contact(name, phone, email, address):
+    """
+    This function is responsible for adding new contacts into the csv file
+    :param name:
+    :param phone:
+    :param email:
+    :param address:
+    :return:
+    """
     contact_details = [name, phone, email, address]
     with open(FILE_STORAGE_PATH, "a") as data_store_file:
         data_store_file.write(",".join(contact_details) + '\n')
@@ -24,6 +40,11 @@ def add_contact(name, phone, email, address):
 
 
 def delete_contact(name):
+    """
+    This function is responsible for deleting a contact
+    :param name: the specified name that the user want to delete it's contact
+    :return: return the contacts without the deleted contact
+    """
     contacts = []
     fields = get_individual_fields()
     for row in fields:
@@ -38,6 +59,14 @@ def delete_contact(name):
 
 
 def update_contact(name, phone, email, address):
+    """
+    This function is responsible for updating the details of an existing contact depending on the name entered
+    :param name:
+    :param phone:
+    :param email:
+    :param address:
+    :return: Updated contact details
+    """
     contact_details = [name, phone, email, address]
     fields = get_individual_fields()
     with open(FILE_STORAGE_PATH, "w") as data_store_file:
@@ -50,7 +79,13 @@ def update_contact(name, phone, email, address):
 
 
 def search_contact(name):
+    """
+    This function is responsible for searching different contacts depending on the name entered
+    :param name:
+    :return: The contact details of the specified name
+    """
     fields = get_individual_fields()
+    # if the entered name does not match the stored contacts, contacts_found is false
     contacts_found = False
     for row in fields:
         if row[0] == name:

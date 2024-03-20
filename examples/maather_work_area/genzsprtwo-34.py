8a7af08dfb83475243887750f0e3d45783b7c4fd
Maather Yasse Mohamed AlJabri <71526@omantel.om>
2024-03-20 10:32:31 +0400

    add dir organizer

---
+++ examples/maather_work_area/genzsprtwo-34.py
@@ -0,0 +1,25 @@
+import os
+_DIRECTORY = "./dir_organizer_files/"
+
+def directory_organizer(directory):
+    try:
+        contents = os.listdir(_DIRECTORY)
+        files = [content for content in contents if os.path.isfile(os.path.join(directory, content))]
+    except OSError as e:
+        print((f"error:{e}"))
+        return
+
+    for file in files:
+        if file.endswith(".txt"):
+            os.makedirs(_DIRECTORY + "text", exist_ok=True)
+            os.rename(_DIRECTORY + file, _DIRECTORY + "text/" + file )
+        elif file.endswith(".img"):
+            os.makedirs(_DIRECTORY + "images", exist_ok=True)
+            os.rename(_DIRECTORY + file, _DIRECTORY + "images/" + file )
+        else:
+            os.makedirs(_DIRECTORY + "misc", exist_ok=True)
+            os.rename(_DIRECTORY + file, _DIRECTORY + "misc/" + file )
+
+
+directory_organizer(_DIRECTORY)
+

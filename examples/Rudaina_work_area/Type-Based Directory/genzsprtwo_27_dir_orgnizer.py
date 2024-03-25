539f43291986d49c59584a02692956d3e23d1371
Rudainasaleh <rudainasaleh14@gmail.com>
2024-03-25 11:05:45 +0400

    Genzsprtwo-27 : File Type-Based Directory Organizer (UPDATED)

--- examples/Rudaina_work_area/Type-Based Directory/genzsprtwo_27_dir_orgnizer.py
+++ examples/Rudaina_work_area/Type-Based Directory/genzsprtwo_27_dir_orgnizer.py
@@ -1,42 +1,28 @@
 import os
+import shutil
+import glob
 
-directory = "./dir"  # the path to the parent dir
-new_txt_path = os.path.join(directory, "./Text")   # path to the Text dir inside the parent dir
-new_img_path = os.path.join(directory, "./Images")  # path to the Images dir inside the parent dir
-miscellaneous = os.path.join(directory, "./Miscellaneous")
-
-files = os.listdir(directory)  # get all files from the parent dir
-
-os.mkdir(new_txt_path)  # create a new directory for .jpg files
-os.mkdir(new_img_path)  # create a new directory for .jpg files
-os.mkdir(miscellaneous)  # create a new directory for other files
+DIRECTORY = "./dir"
 
 
-txt_file_num = 0
-jpg_file_num = 0
-miscellaneous_num = 0
-for file in files:
-    if file.endswith(".txt") or file.endswith(".pdf"):  # if .txt file move it to Text dir
-        src = os.path.join(directory, file)
-        dst = os.path.join(new_txt_path, file)
-        os.rename(src, dst)
-        txt_file_num += 1
+def dir_organizer(source_dir):
+    new_txt_path = os.path.join(source_dir, "./Text")
+    new_img_path = os.path.join(source_dir, "./Images")
+    miscellaneous = os.path.join(source_dir, "./Miscellaneous")
+    for directory in [new_img_path, new_txt_path, miscellaneous]:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    elif file.endswith(".jpg") or file.endswith(".png"):  # if its .jpg  move it to Images dir
-        src = os.path.join(directory, file)
-        dst = os.path.join(new_img_path, file)
-        os.rename(src, dst)
-        jpg_file_num += 1
+    files = glob.glob(os.path.join(source_dir, '*'))
 
-    else:  # any other file move it to miscellaneous dir
-        src = os.path.join(directory, file)
-        dst = os.path.join(miscellaneous, file)
-        os.rename(src, dst)
-        miscellaneous_num += 1
+    for file in files:
+        if os.path.isfile(file):
+            if file.endswith(('.txt', '.pdf')):
+                shutil.move(file, os.path.join(new_txt_path, os.path.basename(file)))
+            elif file.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+                shutil.move(file, os.path.join(new_img_path, os.path.basename(file)))
+            else:
+                shutil.move(file, os.path.join(miscellaneous, os.path.basename(file)))
 
 
-print("Summary:")
-print("-------")
-print(f'- Moved {txt_file_num} .txt files to directory: {directory}/Text')
-print(f'- Moved {jpg_file_num} .jpg files to directory: {directory}/Images')
-print(f'- Moved {miscellaneous_num} other files to directory: {directory}/Miscellaneous')
+dir_organizer(DIRECTORY)

f467362d5a51f352573f93ccd490700b8a36ee43
Maather Yasse Mohamed AlJabri <71526@omantel.om>
2024-03-25 10:36:40 +0400

    use shutil and glob

--- examples/maather_work_area/genzsprtwo-34.py
+++ examples/maather_work_area/genzsprtwo-34.py
@@ -1,25 +1,27 @@
 import os
+import shutil
+import glob
+
 _DIRECTORY = "./dir_organizer_files/"
 
 def directory_organizer(directory):
     try:
-        contents = os.listdir(_DIRECTORY)
-        files = [content for content in contents if os.path.isfile(os.path.join(directory, content))]
+        files = glob.glob(os.path.join(directory, "*"))
     except OSError as e:
-        print((f"error:{e}"))
+        print(f"error: {e}")
         return
 
     for file in files:
-        if file.endswith(".txt"):
-            os.makedirs(_DIRECTORY + "text", exist_ok=True)
-            os.rename(_DIRECTORY + file, _DIRECTORY + "text/" + file )
-        elif file.endswith(".img", ".png", ".jpeg"):
-            os.makedirs(_DIRECTORY + "images", exist_ok=True)
-            os.rename(_DIRECTORY + file, _DIRECTORY + "images/" + file )
-        else:
-            os.makedirs(_DIRECTORY + "misc", exist_ok=True)
-            os.rename(_DIRECTORY + file, _DIRECTORY + "misc/" + file )
+        if os.path.isfile(file):
+            if file.endswith(".txt"):
+                destination = os.path.join(directory, "text")
+            elif file.endswith((".img", ".png", ".jpeg")):
+                destination = os.path.join(directory, "images")
+            else:
+                destination = os.path.join(directory, "misc")
+
+            os.makedirs(destination, exist_ok=True)
+            shutil.move(file, os.path.join(destination, os.path.basename(file)))
 
 
 directory_organizer(_DIRECTORY)
-

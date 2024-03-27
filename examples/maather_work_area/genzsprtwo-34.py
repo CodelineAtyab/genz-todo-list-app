a6a00f5eb18457824eb5fa2d815406d63a23f5ab
Maather Yasse Mohamed AlJabri <71526@omantel.om>
2024-03-27 09:47:14 +0400

    use shutil

--- examples/maather_work_area/genzsprtwo-34.py
+++ examples/maather_work_area/genzsprtwo-34.py
@@ -1,27 +1,25 @@
 import os
 import shutil
-import glob
 
 _DIRECTORY = "./dir_organizer_files/"
 
 def directory_organizer(directory):
     try:
-        files = glob.glob(os.path.join(directory, "*"))
+        contents = os.listdir(directory)
+        files = [content for content in contents if os.path.isfile(os.path.join(directory, content))]
     except OSError as e:
         print(f"error: {e}")
         return
 
     for file in files:
-        if os.path.isfile(file):
-            if file.endswith(".txt"):
-                destination = os.path.join(directory, "text")
-            elif file.endswith((".img", ".png", ".jpeg")):
-                destination = os.path.join(directory, "images")
-            else:
-                destination = os.path.join(directory, "misc")
-
-            os.makedirs(destination, exist_ok=True)
-            shutil.move(file, os.path.join(destination, os.path.basename(file)))
-
+        if file.endswith(".txt"):
+            target_dir = os.path.join(directory, "text")
+        elif file.endswith((".img", ".png", ".jpeg")):
+            target_dir = os.path.join(directory, "images")
+        else:
+            target_dir = os.path.join(directory, "misc")
+        
+        os.makedirs(target_dir, exist_ok=True)
+        shutil.move(os.path.join(directory, file), os.path.join(target_dir, file))
 
 directory_organizer(_DIRECTORY)

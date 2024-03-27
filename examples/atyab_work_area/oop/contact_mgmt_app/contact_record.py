5d737058c610e4a03b7ce9ff97ed803ece0405f8
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2024-03-27 09:49:54 +0400

    Added getters and setters

--- examples/atyab_work_area/oop/contact_mgmt_app/contact_record.py
+++ examples/atyab_work_area/oop/contact_mgmt_app/contact_record.py
@@ -1,6 +1,11 @@
 class ContactRecord:
     def __init__(self, phone_number, name, email):
-        self.phone_number = phone_number
-        self.name = name
-        self.email = email
+        self.__phone_number = phone_number
+        self.__name = name
+        self.__email = email
 
+    def get_phone_number(self):
+        return self.__phone_number
+
+    def set_phone_number(self, inp_phone_number):
+        self.__phone_number = inp_phone_number

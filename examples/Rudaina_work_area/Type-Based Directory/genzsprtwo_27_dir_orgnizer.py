25c776042ec266d4ace92b8f25cb98adb55c5a0d
Radina Al Rahbi <rudainasaleh14@gmail.com>
2024-03-27 11:02:27 +0400

    Genzsprtwo-27 : File Type-Based Directory Organizer (#157)
    * Genzsprtwo-27 : File Type-Based Directory Organizer
    
    * Genzsprtwo-27 : File Type-Based Directory Organizer (UPDATED)
    
    * Genzsprtwo-27 : Update 2 -  File Type-Based Directory Organizer

---
+++ examples/Rudaina_work_area/Type-Based Directory/genzsprtwo_27_dir_orgnizer.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+import glob
+
+source_dir = "source_dir"
+
+new_txt_path = os.path.join(source_dir, "./Text")
+new_img_path = os.path.join(source_dir, "./Images")
+miscellaneous = os.path.join(source_dir, "./Miscellaneous")
+for directory in [new_img_path, new_txt_path, miscellaneous]:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+files = glob.glob(os.path.join(source_dir, '*'))
+for file in files:
+    if os.path.isfile(file):
+        if file.endswith(('.txt', '.pdf')):
+            shutil.move(file, os.path.join(new_txt_path, os.path.basename(file)))
+        elif file.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+            shutil.move(file, os.path.join(new_img_path, os.path.basename(file)))
+        else:
+            shutil.move(file, os.path.join(miscellaneous, os.path.basename(file)))

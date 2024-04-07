27fa1224e9c69d39a714ffadfab724f8837c64d6
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2024-04-07 11:10:29 +0400

    Add a REST API resource called ContactRecords

--- examples/atyab_work_area/my_first_server_app.py
+++ examples/atyab_work_area/my_first_server_app.py
@@ -1,17 +1,54 @@
 import cherrypy
-from filing_crud.file_manager import store_row_in_file
+import os
 
 
-class HelloWorld(object):
-    @cherrypy.expose
+class Root(object):
+    @cherrypy.expose()
     def index(self):
-        return "Hello from Atyab!"
+        pass
 
-    @cherrypy.expose
-    def home(self, **kwargs):
-        store_row_in_file(kwargs['joke'])
-        return f"Joke stored successfully!"
+
+class ContactRecordsV1(object):
+    exposed = True
+
+    def GET(self, phone_number):
+        return f"Handled READ request with this phone number: {phone_number}"
+
+
+    @cherrypy.tools.json_in()
+    def POST(self, *args, **kwargs):
+        return f"Handled CREATE request with this data: {cherrypy.request.json}"
+
+    @cherrypy.tools.json_in()
+    def PUT(self, phone_number):
+        return f"Handled UPDATE request with this data: {cherrypy.request.json}"
+
+
+    def DELETE(self, phone_number):
+        return f"Handled REMOVE request with this data: {phone_number}"
 
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8080})
-cherrypy.quickstart(HelloWorld())
+
+# Mount the ContactsAPI application
+cherrypy.tree.mount(ContactRecordsV1(), '/api/v1/contacts', {
+    '/': {
+        'request.dispatch': cherrypy.dispatch.MethodDispatcher(),  # Use method-based dispatching
+        'tools.sessions.on': False,
+        'tools.response_headers.on': True,
+        'tools.response_headers.headers': [('Content-Type', 'text/plain')]
+    }
+})
+
+cherrypy.tree.mount(Root(), '/', {
+    '/': {
+            'tools.staticdir.root': os.path.abspath(os.path.dirname(__file__)),
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': 'static',
+            'tools.staticdir.index': 'index.html',
+    }
+})
+
+# Start the CherryPy server
+cherrypy.engine.start()
+cherrypy.engine.block()

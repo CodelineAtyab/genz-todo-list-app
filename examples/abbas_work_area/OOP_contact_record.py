428dccc9c6d036e0783138a053568cb0cf055428
Abbas-Abdulrab <134558688+Abbas-Abdulrab@users.noreply.github.com>
2024-04-08 12:04:11 +0400

    GenZ-7:OOP Contact Book (#181)
    * Created and implemented contact book and data store classes
    
    * OOP contact book
    
    * added comments
    
    ---------
    
    Co-authored-by: Abbas Tawfiq Ali Abdulrab <71519@omantel.om>

---
+++ examples/abbas_work_area/OOP_contact_record.py
@@ -0,0 +1,6 @@
+class ContactRecord:
+    def __init__(self, add_name, add_contact, add_email, add_address):
+        self.add_name = add_name
+        self.add_contact = add_contact
+        self.add_email = add_email
+        self.add_address = add_address

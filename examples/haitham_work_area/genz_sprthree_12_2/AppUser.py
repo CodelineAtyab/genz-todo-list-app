a615cb636b0a4962f2929c29a9938d01b28a45eb
Haitham Yasse Hamood Al Maamari <71521@omantel.om>
2024-04-16 21:10:39 +0400

    update 1

--- examples/haitham_work_area/genz_sprthree_12_2/AppUser.py
+++
@@ -1,7 +0,0 @@
-from CSVContactBook import CSVContactBook
-
-class AppUser:
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-        self.contact_book = CSVContactBook()
--- examples/haitham_work_area/genz_sprthree_12_2/Contact.py
+++
@@ -1,6 +0,0 @@
-class Contact:
-    def __init__(self, name, phone, email, address):
-        self.name = name
-        self.phone = phone
-        self.email = email
-        self.address = address

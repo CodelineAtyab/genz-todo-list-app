6d83555cdc1ccecfd9b5626a5c8473ef4bf0708d
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2024-04-18 08:20:39 +0400

    Refactored the code and used cbms as a package in our web api

--- examples/atyab_work_area/contact_book_api/controller/contact_records_controller.py
+++ examples/atyab_work_area/contact_book_api/controller/contact_records_controller.py
@@ -1,6 +1,6 @@
 import cherrypy
 
-from service.contact_records_service import data_store_dict
+import cbms.contact_book_service as contact_book_service_instance
 
 
 class ContactRecordsV1(object):
@@ -8,17 +8,7 @@ class ContactRecordsV1(object):
 
     @cherrypy.tools.json_out()
     def GET(self, phone_number=None):
-        res_msg = {"status": "FAIL", "data": ""}
-        if not phone_number:
-            res_msg["data"] = data_store_dict
-            res_msg["status"] = "SUCCESS"
-        else:
-            try:
-                res_msg["data"] = data_store_dict[phone_number]
-                res_msg["status"] = "SUCCESS"
-            except Exception:
-                res_msg["error"] = "Unable to fetch the record."
-
+        res_msg = {"status": "FAIL", "data": [rec.__dict__ for rec in contact_book_service_instance.contact_book.contact_records]}
         return res_msg
 
 
@@ -26,21 +16,16 @@ class ContactRecordsV1(object):
     @cherrypy.tools.json_out()
     def POST(self, *args, **kwargs):
         res_msg = {"status": "FAIL", "data": ""}
-        try:
-            req_data_dict = cherrypy.request.json
-            data_store_dict[req_data_dict["phone_number"]] = {"name": req_data_dict["name"],
-                                                              "email": req_data_dict["email"]}
-            res_msg["status"] = "SUCCESS"
-        except Exception:
-            res_msg["error"] = "Unable to create the record."
-
         return res_msg
 
     @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
     def PUT(self, phone_number="00000000"):
-        return f"Handled UPDATE request with this data: {cherrypy.request.json}"
+        res_msg = {"status": "FAIL", "data": ""}
+        return res_msg
 
-
+    @cherrypy.tools.json_out()
     def DELETE(self, phone_number="00000000"):
-        return f"Handled REMOVE request with this data: {phone_number}"
+        res_msg = {"status": "FAIL", "data": ""}
+        return res_msg
 

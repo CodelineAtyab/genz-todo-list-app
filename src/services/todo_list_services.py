79a5d893a6473a304b9e0a898d2c8c5526409e6d
Sara Yasser Amur Al Shukaili <71524@omantel.om>
2024-04-18 10:02:36 +0400

    Edited logic for filtering items

--- src/services/todo_list_services.py
+++ src/services/todo_list_services.py
@@ -19,7 +19,9 @@ for line in list_of_lines_in_file:
 todo_list.append_item(Item("Buy groceries", "pending"))
 todo_list.append_item(Item("Complete homework", "in progress"))
 todo_list.append_item(Item("Complete homewoddddrk", "in progrggess"))
-print(todo_list.filter_items("pending"))
+
+if __name__ == "__main__":
+    print(todo_list.filter_items("pending"))
 
 
 

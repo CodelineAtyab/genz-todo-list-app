8cda4e79b2063015fbc0eaf26b79982e90cbffde
Maather AlJabri <85490284+aljab017@users.noreply.github.com>
2024-04-20 17:29:00 +0400

    add api implementation

---
+++ examples/maather_work_area/genzsprfour-19/cbm/contact_book.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from cbm.contact_record import ContactRecord
+
+class ContactBook(ABC):
+    contact_book_data_dict = {}
+
+    @abstractmethod
+    def load_contacts(self):
+        pass
+
+    @abstractmethod
+    def store_contacts(self, list_of_contacts):
+        pass
+
+
+        

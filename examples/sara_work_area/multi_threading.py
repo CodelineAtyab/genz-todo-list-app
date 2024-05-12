3c267e8df8443289436cff1582b037c259f88044
sarah-alshukaily <128701571+sarah-alshukaily@users.noreply.github.com>
2024-05-12 08:44:11 +0400

    Ticket 86ep9k4kw: Threading (#222)
    * Added logic for multithreading in Python
    
    * Edited the multithreading logic
    
    ---------
    
    Co-authored-by: Sara Yasser Amur Al Shukaili <71524@omantel.om>

---
+++ examples/sara_work_area/multi_threading.py
@@ -0,0 +1,20 @@
+import time
+import threading
+
+
+def print_time():
+    while True:
+        print(time.time())
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    try:
+        bg_thread = threading.Thread(target=print_time)
+        bg_thread.daemon = True
+        bg_thread.start()
+    except Exception as ex:
+        print("An error occurred: ", ex)
+
+    user_input = input("Type your message: ")
+    print("Thank you for your input!")
